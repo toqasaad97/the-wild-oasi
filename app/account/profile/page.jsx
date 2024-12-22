c558e4ff598375b075f293a8e38ef466716cbf5b
toqasaad97 <toqasaad1997@gmail.com>
2024-12-22 22:14:28 +0200

    update

--- app/account/profile/page.jsx
+++ app/account/profile/page.jsx
@@ -29,7 +29,7 @@ export default async function Page() {
           name="nationality"
           id="nationality"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-    
+
         />
       </UpdateProfileForm>
     </div>
